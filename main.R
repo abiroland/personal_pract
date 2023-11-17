f5cc2e8432a33ec92d7035fcdcde9699afe14bb2
Roland <ra0033a@american.edu>
2023-11-17 11:19:35 -0500

    main app

---
+++ main.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(shinycssloaders)
+library(shinydashboard)
+library(shinythemes)
+library(tidyverse)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
